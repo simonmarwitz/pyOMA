4a3b54a4257efcb19c3da716cefba6d97531fa08
Simon Marwitz <simon.jakob.marwitz@uni-weimar.de>
2021-12-02 11:33:30 +0100

    processBar helper for printing progress in any method

--- core/Helpers.py
+++ core/Helpers.py
@@ -28,6 +28,30 @@ def nearly_equal(a, b, sig_fig=5):
             )
 
 
+def simplePbar(total):
+    '''
+    Divide the range of total in 100 discrete steps
+        if total < 100 i.e. stepsize > 1: some steps may occur twice, printout must occur multiple times
+        if total > 100 i.e. stepsize < 1: there are gaps, where no printout occurs
+    For each call raise the step value by stepsize until step = total*100/total
+    Check if the step is approximately 100
+    
+    For the last call, additionally a carriage return must be printed
+    
+    '''
+    stepsize = 100 / total
+    last = 0
+    ncalls = 0
+    while True:
+        ncalls += 1
+        while ncalls * stepsize // 1 > last:
+            print('.', end='', flush=True)
+            last += 1
+        if ncalls == total:#np.isclose(step, 100):
+            print('', end='\n', flush=True)
+        yield
+
+
 def calc_xyz(az, elev, r=1):
     if np.abs(az) > 2 * np.pi:
         print('You probably forgot to convert to radians ', az)
