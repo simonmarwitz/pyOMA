0dab61bca2134a46e9c5e776f72d3ec9e3d5003f
Simon Marwitz <imon.jakob.marwitz@uni-weimar.de>
2024-09-06 13:04:09 +0200

    Cleanup

--- pyOMA/core/cython_code/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
-
-setup(
-    #ext_modules = cythonize("cython_code/cython_helpers.pyx")
-    ext_modules = cythonize("hello_world.pyx"),
-    include_dirs=[numpy.get_include()]
-)
