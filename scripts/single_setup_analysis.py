854b9c9e08ea12195b5672114b3c0453c29f8c2c
Simon Marwitz <imon.jakob.marwitz@uni-weimar.de>
2024-09-06 13:29:18 +0200

    update tests and test files

--- scripts/single_setup_analysis.py
+++ scripts/single_setup_analysis.py
@@ -43,39 +43,39 @@ geometry_data = GeometryProcessor.load_geometry(
     nodes_file=working_dir / 'grid.txt',
     lines_file=working_dir / 'lines.txt')
 
-if not os.path.exists(result_folder / 'prep_data.npz') or not skip_existing:
-    prep_data = PreProcessSignals.init_from_config(
+if not os.path.exists(result_folder / 'prep_signals.npz') or not skip_existing:
+    prep_signals = PreProcessSignals.init_from_config(
         conf_file=setup_info,
         meas_file=meas_file,
         chan_dofs_file=chan_dofs_file)
 
     if save_results:
-        prep_data.save_state(result_folder / 'prep_data.npz')
+        prep_signals.save_state(result_folder / 'prep_signals.npz')
 else:
-    prep_data = PreProcessSignals.load_state(result_folder / 'prep_data.npz')
+    prep_signals = PreProcessSignals.load_state(result_folder / 'prep_signals.npz')
 
 if not os.path.exists(
         result_folder /
         'modal_data.npz') or not skip_existing:
 
-    modal_data = method.init_from_config(conf_file, prep_data)
+    modal_data = method.init_from_config(conf_file, prep_signals)
     
     if save_results:
         modal_data.save_state(result_folder / 'modal_data.npz')
 else:
     modal_data = method.load_state(
-        result_folder / 'modal_data.npz', prep_data)
+        result_folder / 'modal_data.npz', prep_signals)
 
 if os.path.exists(result_folder / 'stabil_data.npz') and skip_existing:
     stabil_calc = StabilCalc.load_state(
-        result_folder / 'stabil_data.npz', modal_data, prep_data)
+        result_folder / 'stabil_data.npz', modal_data, prep_signals)
 else:
-    stabil_calc = StabilCalc(modal_data, prep_data)
+    stabil_calc = StabilCalc(modal_data, prep_signals)
 stabil_calc.export_results('/usr/scratch4/sima9999/test.txt')
 
 if interactive:
     stabil_plot = StabilPlot(stabil_calc)
-    start_stabil_gui(stabil_plot, modal_data, geometry_data, prep_data)
+    start_stabil_gui(stabil_plot, modal_data, geometry_data, prep_signals)
 
     if save_results:
         stabil_calc.save_state(result_folder / 'stabil_data.npz')
@@ -83,7 +83,7 @@ if interactive:
 if interactive:
 
     mode_shape_plot = ModeShapePlot(
-        prep_data=prep_data,
+        prep_signals=prep_signals,
         stabil_calc=stabil_calc,
         geometry_data=geometry_data,
         modal_data=modal_data)
--- tests/basic_test.py
+++ tests/basic_test.py
@@ -41,22 +41,22 @@ def analysis_chain(tmpdir):
         lines_file='../input_files/lines',
         parent_childs_file='../input_files/parent_childs')
     PreProcessSignals.load_measurement_file = lambda measurement: measurement
-    prep_data = PreProcessSignals.init_from_config(
+    prep_signals = PreProcessSignals.init_from_config(
         conf_file='../input_files/meas_1/setup_info.txt',
         meas_file=measurement,
         chan_dofs_file='../input_files/meas_1/channel_dofs')
 
     # test all functions of PreProcessData
-    prep_data.correct_offset()
-    prep_data.filter_signals(lowpass=12)
-    prep_data.decimate_signals(2)
-    prep_data.psd_welch(200)
-    prep_data.corr_welch(400)
-    prep_data.psd_blackman_tukey(200)
-    prep_data.welch(400)
-    prep_data.sv_psd()
-    prep_data.correlation(n_lags=400)
-    prep_data.sv_psd()
+    prep_signals.correct_offset()
+    prep_signals.filter_signals(lowpass=12)
+    prep_signals.decimate_signals(2)
+    prep_signals.psd_welch(200)
+    prep_signals.corr_welch(400)
+    prep_signals.psd_blackman_tukey(200)
+    prep_signals.welch(400)
+    prep_signals.sv_psd()
+    prep_signals.correlation(n_lags=400)
+    prep_signals.sv_psd()
 
     # for each OMA method
     for method, config in list(zip([PLSCF, PRCE, BRSSICovRef, SSIData, SSIDataMC, VarSSIRef],
@@ -66,13 +66,13 @@ def analysis_chain(tmpdir):
                                     '../input_files/meas_1/ssi_config.txt',
                                     '../input_files/meas_1/ssi_config.txt',
                                     '../input_files/meas_1/varssi_config.txt']))[:]:
-        modal_obj = method.init_from_config(config, prep_data)
+        modal_obj = method.init_from_config(config, prep_signals)
         # run the full analysis from the sample config file
 
         # run the full analysis manually
         # test save and load functions
         modal_obj.save_state(tmpdir + 'test.npz')
-        modal_obj = method.load_state(tmpdir + 'test.npz', prep_data)
+        modal_obj = method.load_state(tmpdir + 'test.npz', prep_signals)
         # pass the loaded object to StabilDiagram and PlotMSH
 
     # test all the functionality of StabilCalc
@@ -111,9 +111,9 @@ def multi_setup_analysis():
 
     meas_files = working_dir.glob('measurement*/')
 
-    skip_existing = True
+    skip_existing = False
     save_results = False
-    interactive = False
+    interactive = True
 
     n_lags = 400
 
@@ -124,49 +124,48 @@ def multi_setup_analysis():
 
         modal_data = PogerSSICovRef()
 
-        for result_folder in meas_files:
+        for result_folder in list(meas_files):
             meas_name = os.path.basename(result_folder)
 
-            if not os.path.exists(result_folder / 'prep_data.npz') \
+            if not os.path.exists(result_folder / 'prep_signals.npz') \
                     or not skip_existing:
 
-                prep_data = PreProcessSignals.init_from_config(
+                prep_signals = PreProcessSignals.init_from_config(
                     conf_file=result_folder / 'setup_info.txt',
                     meas_file=result_folder / (meas_name + '.npy'),
                     chan_dofs_file=result_folder / "channel_dofs.txt",)
-                prep_data.corr_blackman_tukey(n_lags)
+                prep_signals.corr_blackman_tukey(n_lags)
 
                 if save_results:
-                    prep_data.save_state(result_folder / 'prep_data.npz')
+                    prep_signals.save_state(result_folder / 'prep_signals.npz')
             else:
-                prep_data = PreProcessSignals.load_state(
-                    result_folder / 'prep_data.npz')
+                prep_signals = PreProcessSignals.load_state(
+                    result_folder / 'prep_signals.npz')
 
-            modal_data.add_setup(prep_data)
+            modal_data.add_setup(prep_signals)
 
         modal_data.pair_channels()
 
         modal_data.build_merged_subspace_matrix(199)
-        modal_data.compute_state_matrices(max_model_order=100)
-        modal_data.compute_modal_params()
+        modal_data.compute_modal_params(max_model_order=100, max_modes=24)
         
         if save_results:
             modal_data.save_state(result_folder_merged / 'modal_data.npz')
-        prep_data = None
+        prep_signals = None
     else:
         modal_data = PogerSSICovRef.load_state(
             result_folder_merged / 'modal_data.npz',)
-        prep_data = None
+        prep_signals = None
 
     if os.path.exists(result_folder_merged / 'stabil_data.npz') and skip_existing:
         stabil_calc = StabilCalc.load_state(
-            result_folder_merged / 'stabil_data.npz', modal_data, prep_data)
+            result_folder_merged / 'stabil_data.npz', modal_data, prep_signals)
     else:
-        stabil_calc = StabilCalc(modal_data, prep_data)
+        stabil_calc = StabilCalc(modal_data, prep_signals)
     stabil_calc.export_results('/usr/scratch4/sima9999/test.txt')
     if interactive:
         stabil_plot = StabilPlot(stabil_calc)
-        start_stabil_gui(stabil_plot, modal_data, geometry_data, prep_data)
+        start_stabil_gui(stabil_plot, modal_data, geometry_data, prep_signals)
 
         if save_results:
             stabil_calc.save_state(result_folder_merged / 'stabil_data.npz')
@@ -200,43 +199,43 @@ def single_setup_analysis(
 
     if not os.path.exists(
             result_folder /
-            'prep_data.npz') or not skip_existing:
-        prep_data = PreProcessSignals.init_from_config(
+            'prep_signals.npz') or not skip_existing:
+        prep_signals = PreProcessSignals.init_from_config(
             conf_file=setup_info,
             meas_file=meas_file,
             chan_dofs_file=chan_dofs_file)
 
         if save_results:
-            prep_data.save_state(result_folder / 'prep_data.npz')
+            prep_signals.save_state(result_folder / 'prep_signals.npz')
     else:
-        prep_data = PreProcessSignals.load_state(result_folder / 'prep_data.npz')
+        prep_signals = PreProcessSignals.load_state(result_folder / 'prep_signals.npz')
 
     if not os.path.exists(
             result_folder /
             'modal_data.npz') or not skip_existing:
 
-        modal_data = method.init_from_config(conf_file, prep_data)
+        modal_data = method.init_from_config(conf_file, prep_signals)
 
         if save_results:
             modal_data.save_state(result_folder / 'modal_data.npz')
     else:
         modal_data = method.load_state(
-            result_folder / 'modal_data.npz', prep_data)
+            result_folder / 'modal_data.npz', prep_signals)
 
     if os.path.exists(result_folder / 'stabil_data.npz') and skip_existing:
         stabil_calc = StabilCalc.load_state(
-            result_folder / 'stabil_data.npz', modal_data, prep_data)
+            result_folder / 'stabil_data.npz', modal_data, prep_signals)
     else:
-        stabil_calc = StabilCalc(modal_data, prep_data)
+        stabil_calc = StabilCalc(modal_data, prep_signals)
 
     if interactive:
         stabil_plot = StabilPlot(stabil_calc)
-        start_stabil_gui(stabil_plot, modal_data, geometry_data, prep_data)
+        start_stabil_gui(stabil_plot, modal_data, geometry_data, prep_signals)
 
     if save_results:
         stabil_calc.save_state(result_folder / 'stabil_data.npz')
 
-    return prep_data, modal_data, stabil_calc
+    return prep_signals, modal_data, stabil_calc
 
 
 def merge_poser_test(skip_existing = False,
@@ -260,19 +259,19 @@ def merge_poser_test(skip_existing = False,
         
         meas_name = os.path.basename(result_folder)
         
-        prep_data, modal_data, stabil_calc = single_setup_analysis(
+        prep_signals, modal_data, stabil_calc = single_setup_analysis(
             result_folder=result_folder,
             setup_info=result_folder / 'setup_info.txt',
             meas_file=result_folder / (meas_name + '.npy'),
-            conf_file=working_dir / 'varssi_config.txt',
-            method=BRSSICovRef,
+            conf_file=working_dir / 'ssi_config.txt',
+            method=SSIDataMC,
             geometry_data=geometry_data,
             chan_dofs_file=result_folder / "channel_dofs.txt",
             skip_existing=skip_existing,
             save_results=save_results,
             interactive=True)
 
-        merger.add_setup(prep_data, modal_data, stabil_calc)
+        merger.add_setup(prep_signals, modal_data, stabil_calc)
 
     merger.merge()
 
@@ -283,6 +282,7 @@ def merge_poser_test(skip_existing = False,
         mode_shape_plot = ModeShapePlot(geometry_data=geometry_data,
                                         merged_data=merger)
         start_msh_gui(mode_shape_plot)
+
 
 
 if __name__ == '__main__':
